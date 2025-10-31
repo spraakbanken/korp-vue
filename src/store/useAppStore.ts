cfa4cf66cad398ab822de76c96bf03d52417f098
Arild Matsson <arild.matsson@gu.se>
2025-10-31 12:07:38 +0100

    Query

--- src/store/useAppStore.ts
+++ src/store/useAppStore.ts
@@ -8,29 +8,24 @@ import { setLang } from "@/core/i18n"
 import type { ActiveSearch } from "./appStore.types"
 import type { Store } from "@/core/model/store"
 
-function paramHandler<T extends object, K extends keyof T, U>(
+/** This converts `a?: A` to `a: A | undefined`, which is needed for correct typing of storeToRefs(). */
+type NormalizeOptional<T> = {
+  [K in keyof T]-?: object extends Pick<T, K> ? Exclude<T[K], undefined> | undefined : T[K]
+}
+
+/** Helper for creating a Writable Computed to proxy an object property. */
+function paramHandler<T extends object, K extends keyof T>(
   params: Partial<T>,
   name: K,
-  getDefault?: () => U,
-  options: {
-    /** Decode param value */
-    decode?: (value?: T[K]) => U
-    /** Encode value for param */
-    encode?: (value: U) => T[K]
-  } = {},
-): WritableComputedRef<U> {
-  const decode = options.decode || ((x) => x as U)
-  const encode = options.encode || ((x) => x as T[K])
+  getDefault?: () => T[K],
+): WritableComputedRef<T[K]> {
   return computed({
-    get: () => decode(params[name]) || (getDefault?.() as U),
-    set: (value) => {
-      const encoded = encode(value)
-      params[name] = encoded != getDefault?.() ? encoded : undefined
-    },
+    get: () => params[name] || (getDefault?.() as T[K]),
+    set: (value) => (params[name] = value != getDefault?.() ? value : undefined),
   })
 }
 
-export const useAppStore = defineStore<"app", Store>("app", () => {
+export const useAppStore = defineStore<"app", NormalizeOptional<Store>>("app", () => {
   const url = useUrlSearchParams<HashParams>("hash-params", {
     stringify: (params) => {
       return params.toString().replace(/=(&|$)/g, "$1")
@@ -41,13 +36,9 @@ export const useAppStore = defineStore<"app", Store>("app", () => {
   /** Last executed search query. */
   const activeSearch = ref<ActiveSearch>()
 
-  // const corpus = computed({
-  //   get: () => (url.corpus ? url.corpus.split(",").sort() : []),
-  //   set: (ids) => (url.corpus = ids.sort().join(",")),
-  // })
-  const corpus = paramHandler(url, "corpus", () => [], {
-    decode: (str) => (str ? str.split(",").sort() : []),
-    encode: (ids) => ids.sort().join(),
+  const corpus = computed({
+    get: () => (url.corpus ? url.corpus.split(",").sort() : []),
+    set: (ids) => (url.corpus = ids.sort().join(",")),
   })
   const hpp = paramHandler(url, "hpp", () => settings["hits_per_page_default"])
   const lang = paramHandler(url, "lang", () => settings["default_language"])
