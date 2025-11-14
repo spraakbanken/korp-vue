c9320b50e01e86b1db42bdd6ef3c7dee7206d55f
Arild Matsson <arild.matsson@gu.se>
2025-11-14 18:20:59 +0100

    Corpus selector use modal

--- src/corpora/CorpusSelector.vue
+++ src/corpora/CorpusSelector.vue
@@ -4,9 +4,15 @@ import { corpusListing, corpusSelection } from "@/core/corpora/corpusListing"
 import { useAppStore } from "@/store/useAppStore"
 import CorpusSelectionDialog from "./CorpusSelectionDialog.vue"
 import { useLocale } from "@/i18n/useLocale"
+import ModalDialog from "@/components/ModalDialog.vue"
+import { ref, watchEffect } from "vue"
+import { useAuth } from "@/auth/useAuth"
 
 const store = useAppStore()
+const auth = useAuth()
 const { locObj } = useLocale()
+
+const selection = ref<string[]>([])
 
 /** Runs after corpus selection has been checked for access etc. */
 function resolveValidation(ids: string[]) {
@@ -18,15 +24,29 @@ function resolveValidation(ids: string[]) {
     () => corpusSelection.pickFrom(corpusListing, store.corpus),
   )
 }
+
+watchEffect(() => (selection.value = store.corpus))
 </script>
 
 <template>
   <CorpusSelectionDialog @resolve="resolveValidation" />
 
-  {{ $t("corpora") }}:
-  <select multiple v-model="store.corpus" size="8">
-    <option v-for="corpus of corpusListing.corpora" :key="corpus.id" :value="corpus.id">
-      {{ locObj(corpus.title) }}
-    </option>
-  </select>
+  <div class="d-grid">
+    <button class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#corpus-selector">
+      {{ store.corpus.length }} {{ $t("corpora") }}
+    </button>
+  </div>
+
+  <ModalDialog id="corpus-selector" :title="$t('corpora')" @close="store.corpus = selection">
+    <select multiple v-model="selection" size="8">
+      <option
+        v-for="corpus of corpusListing.corpora"
+        :key="corpus.id"
+        :value="corpus.id"
+        :disabled="corpus?.limited_access && !auth.hasCredential(corpus.id.toUpperCase())"
+      >
+        {{ locObj(corpus.title) }}
+      </option>
+    </select>
+  </ModalDialog>
 </template>
