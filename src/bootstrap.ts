3ec280453a5404cbd9c344b10384c3ae13b3c67e
Arild Matsson <arild.matsson@gu.se>
2025-11-14 20:12:11 +0100

    Search tabs js

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,8 +1,22 @@
-import { Popover } from "bootstrap"
+import { Popover, Tab } from "bootstrap"
+import type { Directive } from "vue"
 
 /** Directive for initalizing a popover. */
-export const vPopover = {
-  mounted(el: HTMLElement) {
+export const vPopover: Directive<HTMLElement> = {
+  mounted(el) {
     new Popover(el)
+  },
+}
+
+/** Directive for initalizing a tab. */
+export const vTab: Directive<HTMLElement> = {
+  mounted(el) {
+    const trigger = new Tab(el)
+    el.role = "tab"
+    el.dataset.bsToggle = "tab"
+    el.addEventListener("click", (event) => {
+      event.preventDefault()
+      trigger.show()
+    })
   },
 }
