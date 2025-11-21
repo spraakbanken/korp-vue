ceea8d00f9b033ad83f563c301d0e5bb3fb381ae
Arild Matsson <arild.matsson@gu.se>
2025-11-21 12:59:03 +0100

    Result tabs

--- src/results/ResultsPanel.vue
+++ src/results/ResultsPanel.vue
@@ -6,17 +6,67 @@ import StatisticsResult from "./StatisticsResult.vue"
 
 const store = useAppStore()
 const { resultTab } = storeToRefs(store)
+
+const tabOptions = [
+  { key: 1, name: "kwic" },
+  { key: 2, name: "statistics" },
+  { key: 3, name: "wordpicture" },
+]
 </script>
 
 <template>
   <div>
-    <nav>
-      <button @click="resultTab = 1">KWIC</button>
-      <button @click="resultTab = 2">Statistics</button>
+    <!-- Tab bar -->
+    <nav class="nav nav-tabs" id="result-tabs-list">
+      <button
+        v-for="{ key, name } in tabOptions"
+        :key
+        class="nav-link"
+        :class="{ active: resultTab == key }"
+        :id="`result-tabs-tab-${name}`"
+        v-tab
+        :aria-controls="`result-tabs-pane-${name}`"
+        :aria-selected="resultTab == key"
+        @click="resultTab = key"
+      >
+        {{ $t(`result.${name}`) }}
+      </button>
     </nav>
-    <section>
-      <KwicResults v-show="resultTab == 1" />
-      <StatisticsResult v-show="resultTab == 2" />
-    </section>
+
+    <!-- Tab content -->
+    <div class="tab-content border border-top-0 p-2" id="result-tabs-content">
+      <div
+        class="tab-pane"
+        :class="{ 'show active': resultTab == 1 }"
+        id="result-tabs-pane-kwic"
+        role="tabpanel"
+        aria-labelledby="result-tabs-tab-kwic"
+        tabindex="0"
+      >
+        <KwicResults />
+      </div>
+
+      <div
+        class="tab-pane"
+        :class="{ 'show active': resultTab == 2 }"
+        id="result-tabs-pane-statistics"
+        role="tabpanel"
+        aria-labelledby="result-tabs-tab-statistics"
+        tabindex="0"
+      >
+        <StatisticsResult />
+      </div>
+
+      <div
+        class="tab-pane"
+        :class="{ 'show active': resultTab == 3 }"
+        id="result-tabs-pane-wordpicture"
+        role="tabpanel"
+        aria-labelledby="result-tabs-tab-wordpicture"
+        tabindex="0"
+      >
+        TODO Word picture
+      </div>
+    </div>
   </div>
 </template>
