b9c3c42e54445e8ff59c3e90ed2cdbba97b88649
Arild Matsson <arild.matsson@gu.se>
2026-01-12 13:34:20 +0100

    Slimmer query builder

--- src/search/extended/SearchExtended.vue
+++ src/search/extended/SearchExtended.vue
@@ -42,10 +42,6 @@ function addToken() {
   tokens.push(createToken())
 }
 
-function removeToken(index: number) {
-  if (index !== -1) tokens.splice(index, 1)
-}
-
 function addDisjunction(tokenIndex: number) {
   if (!isCqpToken(tokens[tokenIndex])) throw new Error("Cannot modify non-existing or struct token")
   tokens[tokenIndex].and_block.push([createCondition()])
@@ -53,16 +49,21 @@ function addDisjunction(tokenIndex: number) {
 
 function addCondition(tokenIndex: number, disjunctionIndex: number) {
   if (!isCqpToken(tokens[tokenIndex])) throw new Error("Cannot modify non-existing or struct token")
-  tokens[tokenIndex]!.and_block[disjunctionIndex]!.push(createCondition())
+  tokens[tokenIndex].and_block[disjunctionIndex]!.push(createCondition())
 }
 
 function removeCondition(tokenIndex: number, disjunctionIndex: number, conditionIndex: number) {
   if (!isCqpToken(tokens[tokenIndex])) throw new Error("Cannot modify non-existing or struct token")
-  tokens[tokenIndex]!.and_block[disjunctionIndex]!.splice(conditionIndex, 1)
+  tokens[tokenIndex].and_block[disjunctionIndex]!.splice(conditionIndex, 1)
 
   // If the disjunction is empty, remove it
-  if (tokens[tokenIndex]!.and_block[disjunctionIndex]!.length == 0) {
-    tokens[tokenIndex]!.and_block.splice(disjunctionIndex, 1)
+  if (tokens[tokenIndex].and_block[disjunctionIndex]!.length == 0) {
+    tokens[tokenIndex].and_block.splice(disjunctionIndex, 1)
+  }
+
+  // If the token is empty, remove it
+  if (tokens[tokenIndex].and_block.length == 0) {
+    tokens.splice(tokenIndex, 1)
   }
 }
 
@@ -90,23 +91,13 @@ async function commitSearch() {
 
     <div class="d-flex gap-4 align-items-center overflow-x-auto">
       <div v-for="(token, i) in tokens" :key="i" class="card flex-shrink-0">
-        <div class="card-header d-flex justify-content-between align-items-center">
-          {{ $t("search.extended.token") }}
-          <button
-            type="button"
-            class="btn-close"
-            :aria-label="$t('search.extended.token_remove')"
-            @click="removeToken(i)"
-          ></button>
-        </div>
-
         <!-- 2-dimensional repetition: an AND of OR's-->
-        <div v-if="isCqpToken(token)" class="card-body d-flex flex-column gap-2">
+        <div v-if="isCqpToken(token)" class="card-body p-2 d-flex flex-column gap-2">
           <template v-for="(disjunction, j) in token.and_block" :key="j">
             <div v-if="j > 0">{{ $t("search.and") }}</div>
 
             <div class="card bg-body-tertiary">
-              <div class="card-body d-flex flex-column gap-2">
+              <div class="card-body p-2 d-flex flex-column gap-2">
                 <template v-for="(condition, k) in disjunction" :key="k">
                   <div v-if="k > 0">{{ $t("search.or") }}</div>
 
@@ -133,7 +124,10 @@ async function commitSearch() {
                       :aria-label="$t('search.extended.condition_remove')"
                       @click="removeCondition(i, j, k)"
                       :class="{
-                        invisible: token.and_block.length == 1 && disjunction.length == 1,
+                        invisible:
+                          tokens.length == 1 &&
+                          token.and_block.length == 1 &&
+                          disjunction.length == 1,
                       }"
                       style="width: 0.2rem; background-size: contain"
                     ></button>
@@ -160,10 +154,14 @@ async function commitSearch() {
       </div>
 
       <div>
-        <button type="button" class="btn btn-secondary" @click="addToken()">
+        <button type="button" class="btn btn-secondary text-nowrap" @click="addToken()">
           + {{ $t("search.extended.token_add") }}
         </button>
       </div>
+    </div>
+
+    <div class="small text-muted">
+      {{ $t("search.extended.instructions") }}
     </div>
 
     <input type="submit" :value="$t('search')" class="btn btn-primary" />
