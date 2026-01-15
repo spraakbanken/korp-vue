c900df3c013a39d0a67efe63e48e172e7a5ab9ec
Arild Matsson <arild.matsson@gu.se>
2026-01-15 15:14:42 +0100

    Tweak Extended layout code

--- src/search/extended/QueryBuilder.vue
+++ src/search/extended/QueryBuilder.vue
@@ -78,54 +78,49 @@ function addBoundary(start: boolean) {
 
 <template>
   <div class="d-flex gap-4 align-items-center overflow-x-auto">
-    <div v-for="(token, i) in tokens" :key="i" class="card flex-shrink-0">
+    <div v-for="(token, i) in tokens" :key="i" class="card flex-shrink-0 p-2">
       <!-- 2-dimensional repetition: an AND of OR's-->
-      <div v-if="isCqpToken(token)" class="card-body p-2 d-flex flex-column gap-2">
+      <div v-if="isCqpToken(token)" class="vstack gap-2">
         <template v-for="(disjunction, j) in token.and_block" :key="j">
           <div v-if="j > 0">{{ $t("search.and") }}</div>
 
-          <div class="card bg-body-tertiary">
-            <div class="card-body p-2 d-flex flex-column gap-2">
-              <template v-for="(condition, k) in disjunction" :key="k">
-                <div v-if="k > 0">{{ $t("search.or") }}</div>
+          <div class="card bg-body-tertiary p-2 vstack gap-2">
+            <template v-for="(condition, k) in disjunction" :key="k">
+              <div v-if="k > 0">{{ $t("search.or") }}</div>
 
-                <!-- Each condition (attribute-operator-value) -->
-                <div class="d-flex gap-3 align-items-center">
-                  <div class="flex-grow-1 d-flex flex-column gap-2">
-                    <SearchExtendedAttribute
+              <!-- Each condition (attribute-operator-value) -->
+              <div class="hstack gap-2">
+                <div class="flex-grow-1 vstack gap-1">
+                  <SearchExtendedAttribute :condition @update="(name) => (condition.type = name)" />
+                  <div class="hstack gap-1 align-items-baseline">
+                    <SearchExtendedOperator :condition @update="(op) => (condition.op = op)" />
+                    <SearchExtendedValue
                       :condition
-                      @update="(name) => (condition.type = name)"
+                      @update="(value) => (condition.val = value)"
+                      class="flex-grow-1"
                     />
-                    <div class="d-flex gap-2 align-items-baseline">
-                      <SearchExtendedOperator :condition @update="(op) => (condition.op = op)" />
-                      <SearchExtendedValue
-                        :condition
-                        @update="(value) => (condition.val = value)"
-                        class="flex-grow-1"
-                      />
-                    </div>
                   </div>
-
-                  <button
-                    type="button"
-                    class="btn-close"
-                    :aria-label="$t('search.extended.condition_remove')"
-                    @click="removeCondition(i, j, k)"
-                    :class="{ invisible: !canRemoveCondition }"
-                    style="width: 0.2rem; background-size: contain"
-                  ></button>
                 </div>
-              </template>
 
-              <div>
                 <button
                   type="button"
-                  class="btn btn-link text-decoration-none btn-sm"
-                  @click="addCondition(i, j)"
-                >
-                  + {{ $t("search.or") }}…
-                </button>
+                  class="btn-close"
+                  :aria-label="$t('search.extended.condition_remove')"
+                  @click="removeCondition(i, j, k)"
+                  :class="{ invisible: !canRemoveCondition }"
+                  style="width: 0.2rem; background-size: contain"
+                ></button>
               </div>
+            </template>
+
+            <div>
+              <button
+                type="button"
+                class="btn btn-link text-decoration-none btn-sm"
+                @click="addCondition(i, j)"
+              >
+                + {{ $t("search.or") }}…
+              </button>
             </div>
           </div>
         </template>
@@ -141,55 +136,53 @@ function addBoundary(start: boolean) {
         </div>
       </div>
 
-      <div v-else-if="isCqpStruct(token)" class="card-body p-2">
-        <div class="d-flex gap-3 align-items-center">
-          <div class="d-flex flex-column gap-2">
-            <select class="form-select">
-              <option
-                v-for="(label, tag) in corpusSelection.getCommonWithins()"
-                :key="tag"
-                :value="tag"
-              >
-                {{ $t(`tag.${label}`) }}
-              </option>
-            </select>
+      <div v-else-if="isCqpStruct(token)" class="hstack gap-2">
+        <div class="vstack gap-2">
+          <select class="form-select">
+            <option
+              v-for="(label, tag) in corpusSelection.getCommonWithins()"
+              :key="tag"
+              :value="tag"
+            >
+              {{ $t(`tag.${label}`) }}
+            </option>
+          </select>
 
-            <div>
-              <div class="form-check">
-                <label class="form-check-label">
-                  <input
-                    class="form-check-input"
-                    type="radio"
-                    :name="`boundary-${i}-start`"
-                    :checked="token.start"
-                    @change="token.start = true"
-                  />
-                  {{ $t("search.extended.boundary.start") }}
-                </label>
-              </div>
-              <div class="form-check">
-                <label class="form-check-label">
-                  <input
-                    class="form-check-input"
-                    type="radio"
-                    :name="`boundary-${i}-start`"
-                    :checked="!token.start"
-                    @change="token.start = false"
-                  />
-                  {{ $t("search.extended.boundary.end") }}
-                </label>
-              </div>
+          <div>
+            <div class="form-check">
+              <label class="form-check-label">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  :name="`boundary-${i}-start`"
+                  :checked="token.start"
+                  @change="token.start = true"
+                />
+                {{ $t("search.extended.boundary.start") }}
+              </label>
+            </div>
+            <div class="form-check">
+              <label class="form-check-label">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  :name="`boundary-${i}-start`"
+                  :checked="!token.start"
+                  @change="token.start = false"
+                />
+                {{ $t("search.extended.boundary.end") }}
+              </label>
             </div>
           </div>
-
-          <button
-            type="button"
-            class="btn-close"
-            :aria-label="$t('search.extended.boundary_remove')"
-            @click="removeItem(i)"
-            style="width: 0.2rem; background-size: contain"
-          ></button>
         </div>
+
+        <button
+          type="button"
+          class="btn-close"
+          :aria-label="$t('search.extended.boundary_remove')"
+          @click="removeItem(i)"
+          style="width: 0.2rem; background-size: contain"
+        ></button>
       </div>
 
       <div v-else>TODO Bound? {{ token }}</div>
