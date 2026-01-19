c41d06f2dddded9649c3eb28760b0d6d9bca36de
Arild Matsson <arild.matsson@gu.se>
2026-01-19 16:48:20 +0100

    Extract StatisticsGrid

---
+++ src/results/StatisticsGrid.vue
@@ -0,0 +1,103 @@
+<script setup lang="ts">
+import { StatsProxy } from "@/core/backend/proxy/StatsProxy"
+import type { Corpus } from "@/core/config/corpusConfig.types"
+import { corpusSelection } from "@/core/corpora/corpusListing"
+import { getCqp } from "@/core/statistics/statistics"
+import {
+  isTotalRow,
+  type Row,
+  type SearchParams,
+  type SingleRow,
+} from "@/core/statistics/statistics.types"
+import { ExampleTask } from "@/core/task/ExampleTask"
+import { useAppStore } from "@/store/useAppStore"
+import { computed, onMounted, ref, useTemplateRef } from "vue"
+import { useI18n } from "vue-i18n"
+
+const props = defineProps<{
+  params: SearchParams
+  rows: Row[]
+}>()
+
+const $emit = defineEmits<{
+  (e: "subsearch", task: ExampleTask): void
+}>()
+
+const store = useAppStore()
+const { t } = useI18n()
+
+const attrs = ref<string[]>(["word"])
+const corpora = ref<Corpus[]>(corpusSelection.corpora)
+const cqp = computed(() => store.activeSearch?.cqp || "[]")
+const gridEl = useTemplateRef("grid")
+
+const proxy = new StatsProxy()
+
+onMounted(async () => {
+  if (!gridEl.value) throw new Error("Grid element missing")
+
+  const statisticsGridModule = await import("@/core/statistics/statistics-grid")
+  const { StatisticsGrid } = statisticsGridModule
+  const grid = new StatisticsGrid(
+    gridEl.value,
+    props.rows,
+    corpora.value.map((c) => c.id.toUpperCase()),
+    attrs.value,
+    store,
+    t("result.statistics.total"),
+    () => {}, // TODO Corpus distribution chart
+    onValueClick,
+  )
+  grid.render()
+  grid.resizeCanvas()
+  grid.autosizeColumns()
+})
+
+/** Open an subsearch tab when clicking a frequency value */
+function onValueClick(row: Row, corpusId?: string) {
+  const cqps = [cqp.value]
+
+  // Add a subquery CQP matching a value row
+  if (!isTotalRow(row)) cqps.push(buildExampleCqp(row))
+
+  // Unless corpus is given, find which corpora had any hits (uppercase ids)
+  const corpora = corpusId
+    ? [corpusId]
+    : Object.keys(row.count).filter((id) => row.count[id]![0] > 0)
+
+  const task = new ExampleTask(corpora, cqps, proxy.getParams().default_within, store.reading_mode)
+  $emit("subsearch", task)
+}
+
+/** Create sub query for a given value row */
+function buildExampleCqp(row: SingleRow) {
+  // isPhraseLevelDisjunction can be set in custom code for constructing cqp like: ([] | [])
+  if ("isPhraseLevelDisjunction" in row && row.isPhraseLevelDisjunction) {
+    // In this case the statsValues array is one level deeper
+    const statsValues = row.statsValues as unknown as Record<string, string[]>[][]
+    const tokens = statsValues.map((vals) => getCqp(vals, props.params.ignoreCase))
+    return tokens.join(" | ")
+  }
+
+  // Normal case
+  return getCqp(row.statsValues, props.params.ignoreCase)
+}
+</script>
+
+<template>
+  <div>
+    <div ref="grid" role="grid"></div>
+  </div>
+</template>
+
+<style scoped>
+div[role="grid"] {
+  height: 30em;
+}
+:deep(div[role="columnheader"]) {
+  background-color: rgb(var(--bs-tertiary-bg-rgb));
+}
+:deep(.slick-cell input[type="checkbox"]) {
+  margin-left: 2px;
+}
+</style>
--- src/results/StatisticsResult.vue
+++ src/results/StatisticsResult.vue
@@ -2,19 +2,15 @@
 import { StatsProxy } from "@/core/backend/proxy/StatsProxy"
 import type { Corpus } from "@/core/config/corpusConfig.types"
 import { corpusSelection } from "@/core/corpora/corpusListing"
-import { getCqp, processStatisticsResult } from "@/core/statistics/statistics"
-import {
-  isTotalRow,
-  type Row,
-  type SingleRow,
-  type StatisticsProcessed,
-} from "@/core/statistics/statistics.types"
+import { processStatisticsResult } from "@/core/statistics/statistics"
+import { type StatisticsProcessed } from "@/core/statistics/statistics.types"
 import { ExampleTask } from "@/core/task/ExampleTask"
 import { useAppStore } from "@/store/useAppStore"
 import { whenever } from "@vueuse/core"
-import { computed, ref, useTemplateRef, watch } from "vue"
+import { computed, ref } from "vue"
 import { useI18n } from "vue-i18n"
 import { useDynamicTabs } from "./useDynamicTabs"
+import StatisticsGrid from "./StatisticsGrid.vue"
 
 const props = defineProps<{
   active: boolean
@@ -28,7 +24,6 @@ const attrs = ref<string[]>(["word"])
 const corpora = ref<Corpus[]>(corpusSelection.corpora)
 const cqp = computed(() => store.activeSearch?.cqp || "[]")
 const data = ref<StatisticsProcessed>()
-const gridEl = useTemplateRef("grid")
 
 const proxy = new StatsProxy()
 
@@ -52,64 +47,14 @@ async function doSearch() {
   data.value = await processStatisticsResult(originalCorpora, counts, attrsValue, false, cqpValue)
 }
 
-watch(data, async () => {
-  if (!data.value) return
-  if (!gridEl.value) throw new Error("Grid element missing")
-
-  const statisticsGridModule = await import("@/core/statistics/statistics-grid")
-  const { StatisticsGrid } = statisticsGridModule
-  const grid = new StatisticsGrid(
-    gridEl.value,
-    data.value.rows,
-    corpora.value.map((c) => c.id.toUpperCase()),
-    attrs.value,
-    store,
-    t("result.statistics.total"),
-    () => {},
-    onValueClick,
-  )
-  grid.render()
-  grid.resizeCanvas()
-  grid.autosizeColumns()
-})
-
-/** Open an subsearch tab when clicking a frequency value */
-function onValueClick(row: Row, corpusId?: string) {
-  const cqps = [cqp.value]
-
-  // Add a subquery CQP matching a value row
-  if (!isTotalRow(row)) cqps.push(buildExampleCqp(row))
-
-  // Unless corpus is given, find which corpora had any hits (uppercase ids)
-  const corpora = corpusId
-    ? [corpusId]
-    : Object.keys(row.count).filter((id) => row.count[id]![0] > 0)
-
-  const task = new ExampleTask(corpora, cqps, proxy.getParams().default_within, store.reading_mode)
+function onSubsearch(task: ExampleTask) {
   createTab(t("result.kwic"), task)
-}
-
-/** Create sub query for a given value row */
-function buildExampleCqp(row: SingleRow) {
-  if (!data.value) throw new Error("Missing statistics data")
-  const params = data.value.params
-
-  // isPhraseLevelDisjunction can be set in custom code for constructing cqp like: ([] | [])
-  if ("isPhraseLevelDisjunction" in row && row.isPhraseLevelDisjunction) {
-    // In this case the statsValues array is one level deeper
-    const statsValues = row.statsValues as unknown as Record<string, string[]>[][]
-    const tokens = statsValues.map((vals) => getCqp(vals, params.ignoreCase))
-    return tokens.join(" | ")
-  }
-
-  // Normal case
-  return getCqp(row.statsValues, params.ignoreCase)
 }
 </script>
 
 <template>
   <div>
-    <div ref="grid" role="grid"></div>
+    <StatisticsGrid v-if="data" :rows="data.rows" :params="data.params" @subsearch="onSubsearch" />
   </div>
 </template>
 
