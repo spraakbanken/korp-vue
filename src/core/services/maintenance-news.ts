862383a7a947f3d610ba7c23697ad9dfb6263dde
Arild Matsson <arild.matsson@gu.se>
2026-01-23 09:44:48 +0100

    yarn upgrade --latest, lint, format

--- src/core/services/maintenance-news.ts
+++ src/core/services/maintenance-news.ts
@@ -4,17 +4,17 @@ import { locObj } from "@/i18n"
 
 /** Show simple HTML for maintenance-tagged news items. */
 export async function createMaintenanceNewsElement(): Promise<string> {
-    const allItems = await fetchNews()
-    const items = allItems.filter((item) => item.tags?.includes("maintenance"))
-    if (!items.length) return ""
+  const allItems = await fetchNews()
+  const items = allItems.filter((item) => item.tags?.includes("maintenance"))
+  if (!items.length) return ""
 
-    const htmlItems = items.map(
-        (item) => html`
-            <article>
-                <heading><h3>${locObj(item.title)}</h3></heading>
-                <div>${locObj(item.body)}</div>
-            </article>
-        `,
-    )
-    return html`<section class="maintenance-news">${htmlItems.join("\n")}</section>`
+  const htmlItems = items.map(
+    (item) => html`
+      <article>
+        <heading><h3>${locObj(item.title)}</h3></heading>
+        <div>${locObj(item.body)}</div>
+      </article>
+    `,
+  )
+  return html`<section class="maintenance-news">${htmlItems.join("\n")}</section>`
 }
--- src/core/services/matomo.ts
+++ src/core/services/matomo.ts
@@ -5,29 +5,29 @@ import settings from "@/settings"
 
 // Allow environment-specific (development/staging/production) settings or fallback to general settings
 const matomoSettings = {
-    ...settings.matomo,
-    ...settings.matomo?.[process.env.ENVIRONMENT],
+  ...settings.matomo,
+  ...settings.matomo?.[process.env.ENVIRONMENT],
 }
 
 if (matomoSettings.url && matomoSettings.site) {
-    /** Matomo message queue */
-    var _paq = (window._paq = window._paq || [])
-    _paq.push(["trackPageView"])
-    _paq.push(["enableLinkTracking"])
+  /** Matomo message queue */
+  const _paq = (window._paq = window._paq || [])
+  _paq.push(["trackPageView"])
+  _paq.push(["enableLinkTracking"])
 
-    // Load the Matomo client once page has been loaded.
-    ;(function () {
-        var u = matomoSettings["url"]
-        _paq.push(["setTrackerUrl", u + "matomo.php"])
-        _paq.push(["setSiteId", matomoSettings["site"]])
-        var d = document,
-            g = d.createElement("script"),
-            s = d.getElementsByTagName("script")[0]
-        g.type = "text/javascript"
-        g.async = true
-        g.src = u + "matomo.js"
-        s.parentNode!.insertBefore(g, s)
-    })()
+  // Load the Matomo client once page has been loaded.
+  ;(function () {
+    const u = matomoSettings["url"]
+    _paq.push(["setTrackerUrl", u + "matomo.php"])
+    _paq.push(["setSiteId", matomoSettings["site"]])
+    const d = document,
+      g = d.createElement("script"),
+      s = d.getElementsByTagName("script")[0]
+    g.type = "text/javascript"
+    g.async = true
+    g.src = u + "matomo.js"
+    s.parentNode!.insertBefore(g, s)
+  })()
 }
 
 /**
@@ -36,8 +36,8 @@ if (matomoSettings.url && matomoSettings.site) {
  * Does nothing if Matomo is not configured.
  */
 export function matomoSend<P extends keyof Matomo>(command: P, ...args: Parameters<Matomo[P]>) {
-    window._paq = window._paq || []
-    window._paq.push([command, ...args])
+  window._paq = window._paq || []
+  window._paq.push([command, ...args])
 }
 
 /**
@@ -47,8 +47,8 @@ export function matomoSend<P extends keyof Matomo>(command: P, ...args: Paramete
  * Fully described on https://developer.matomo.org/api-reference/tracking-javascript
  */
 export type Matomo = {
-    trackEvent: (category: string, action: string, name?: string, value?: number) => void
-    trackLink: (url: string, linkType: string) => void
-    trackPageView: (customTitle?: string) => void
-    trackSiteSearch: (keyword: string, category?: string, resultsCount?: number) => void
+  trackEvent: (category: string, action: string, name?: string, value?: number) => void
+  trackLink: (url: string, linkType: string) => void
+  trackPageView: (customTitle?: string) => void
+  trackSiteSearch: (keyword: string, category?: string, resultsCount?: number) => void
 }
