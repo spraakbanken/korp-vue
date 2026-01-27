7e86824bad957980413335e8eea8c5f4158f0ea7
Arild Matsson <arild.matsson@gu.se>
2026-01-27 16:04:26 +0100

    More reactive QBCondition

--- src/search/extended/QueryBuilderCondition.vue
+++ src/search/extended/QueryBuilderCondition.vue
@@ -1,43 +1,50 @@
 <script setup lang="ts">
 import type { OperatorKorp } from "@/core/cqp/cqp.types"
 import QueryBuilderValue from "./QueryBuilderValue.vue"
-import { ref, useId, watch } from "vue"
+import { computed, ref, useId, watch } from "vue"
 import type { AttributeOption } from "@/core/corpora/CorpusSet"
 import { corpusSelection } from "@/core/corpora/corpusListing"
 import settings, { prefixAttr, unprefixAttr } from "@/core/config"
 import AttributeSelector from "@/AttributeSelector.vue"
 
+/** Model for selected attribute name */
 const attribute = defineModel<string>("attribute", { required: true })
+/** Model for selected operator */
 const operator = defineModel<OperatorKorp>("operator", { required: true })
+/** Model for input value */
 const value = defineModel<string>("value", { required: true })
 
 const inputId = useId()
+/** Available attribute options */
 const attributeOptions = ref<AttributeOption[]>([])
-const operatorOptions = ref<Record<string, OperatorKorp>>(settings["default_options"])
+/** Attribute object matching the currently selected attribute name */
+const attributeObject = computed(() =>
+  attributeOptions.value.find((attr) => attr.name == unprefixAttr(attribute.value)),
+)
+/** Available operator options for the selected attribute */
+const operatorOptions = computed(() => attributeObject.value?.opts || settings["default_options"])
 
-const findOption = (name: string) =>
-  attributeOptions.value.find((option) => option.name == unprefixAttr(name))
-
+/** Update available attributes */
 function refresh() {
   if (!corpusSelection.corpora.length) return
-
-  // Update available attributes
   attributeOptions.value = corpusSelection.getAttributeGroupsExtended()
-
-  // If selected attribute is no longer available, reset selection
-  const attributeObject = findOption(attribute.value)
-  if (!attribute.value || !attributeObject) attribute.value = "word"
-
-  // Update available operators
-  operatorOptions.value = attributeObject?.opts || settings["default_options"]
-  const operators = Object.values(operatorOptions.value)
-
-  // If current operator is not available, select first option.
-  if (!operator.value || !operators.includes(operator.value)) operator.value = operators[0]!
 }
 
+// Set attribute options initially and on corpus selection changes
 refresh()
 corpusSelection.listen(refresh)
+
+watch(attributeObject, () => {
+  // If selected attribute is no longer available, reset selection
+  if (!attributeObject.value) attribute.value = "word"
+})
+
+watch(operatorOptions, () => {
+  // If selected operator does not exist for the current (newly selected) attribute, select first operator
+  const ops = Object.values(operatorOptions.value)
+  if (!operator.value || !ops.includes(operator.value)) operator.value = ops[0]!
+})
+
 watch(attribute, refresh)
 </script>
 
@@ -48,9 +55,10 @@ watch(attribute, refresh)
       <label :for="`${inputId}-attr`" class="visually-hidden">
         {{ $t("search.extended.attribute") }}
       </label>
+      <!-- Instead of the v-model syntax, use v-bind and event handler explicitly to convert between our `string` model and the child component's `Attribute` model. -->
       <AttributeSelector
         :options="attributeOptions"
-        :model-value="findOption(attribute)"
+        :model-value="attributeObject"
         @update:model-value="(attr) => (attribute = attr ? prefixAttr(attr) : '')"
         :id="`${inputId}-attr`"
       />
