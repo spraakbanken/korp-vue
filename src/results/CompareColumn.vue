6cc8396532b49ed86d0c160aa61a3d2e29705128
Arild Matsson <arild.matsson@gu.se>
2026-01-28 15:52:09 +0100

    Click compare row

--- src/results/CompareColumn.vue
+++ src/results/CompareColumn.vue
@@ -1,28 +1,35 @@
 <script setup lang="ts">
 import type { CompareItem } from "@/core/task/CompareTask"
 
-const props = defineProps<{
+defineProps<{
   items: CompareItem[]
   max: number
-  polarity: "positive" | "negative"
+  left?: boolean
 }>()
 
-const isLeft = props.polarity === "negative"
+defineEmits<{
+  (e: "select", item: CompareItem): void
+}>()
 </script>
 
 <template>
   <ul class="list-group">
-    <li v-for="item in items" :key="item.key" class="list-group-item position-relative">
+    <li
+      v-for="item in items"
+      :key="item.key"
+      class="list-group-item position-relative link"
+      @click="$emit('select', item)"
+    >
+      <!-- Background bar -->
       <div
         class="position-absolute h-100 top-0"
-        :class="isLeft ? 'bg-info-subtle end-0' : 'bg-warning-subtle start-0'"
+        :class="left ? 'bg-info-subtle end-0' : 'bg-warning-subtle start-0'"
         :style="{ width: `${Math.abs(item.loglike / max) * 100}%` }"
       ></div>
+      <!-- Text -->
       <div class="d-flex justify-content-between align-items-baseline position-relative z-1">
         {{ String(item.tokenLists) }}
-        <span class="text-muted">
-          {{ item.abs }}
-        </span>
+        <span class="text-muted">{{ item.abs }}</span>
       </div>
     </li>
   </ul>
--- src/results/CompareResults.vue
+++ src/results/CompareResults.vue
@@ -1,22 +1,42 @@
 <script setup lang="ts">
-import type { CompareResult, CompareTask } from "@/core/task/CompareTask"
+import { useI18n } from "vue-i18n"
 import { computedAsync } from "@vueuse/core"
+import type { CompareItem, CompareResult, CompareTask } from "@/core/task/CompareTask"
 import CompareColumn from "./CompareColumn.vue"
+import { useDynamicTabs } from "./useDynamicTabs"
 
 const props = defineProps<{ task: CompareTask }>()
-const response = computedAsync<CompareResult>(() => props.task.send())
+
+const { createTab } = useDynamicTabs()
+const { t } = useI18n()
+
+const result = computedAsync<CompareResult>(() => props.task.send())
+
+function clickItem(side: 0 | 1, item: CompareItem) {
+  const exampleTask = props.task.createExampleTask(side, item)
+  createTab(t("result.kwic"), exampleTask)
+}
 </script>
 
 <template>
   <div class="vstack gap-2">
-    <div v-if="response" class="row">
+    <div v-if="result" class="row">
       <div class="col-sm-6">
-        <h3>{{ $t("result.compare.column_heading", { label: response.cmp1.label }) }}</h3>
-        <CompareColumn :items="response.tables.negative" :max="response.max" polarity="negative" />
+        <h3>{{ $t("result.compare.column_heading", { label: result.cmp1.label }) }}</h3>
+        <CompareColumn
+          :items="result.tables.negative"
+          :max="result.max"
+          left
+          @select="(item) => clickItem(0, item)"
+        />
       </div>
       <div class="col-sm-6">
-        <h3>{{ $t("result.compare.column_heading", { label: response.cmp2.label }) }}</h3>
-        <CompareColumn :items="response.tables.positive" :max="response.max" polarity="positive" />
+        <h3>{{ $t("result.compare.column_heading", { label: result.cmp2.label }) }}</h3>
+        <CompareColumn
+          :items="result.tables.positive"
+          :max="result.max"
+          @select="(item) => clickItem(1, item)"
+        />
       </div>
     </div>
   </div>
