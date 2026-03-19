78713e5abb59ba213c7dfddeeb864eb910782130
Arild Matsson <arild.matsson@gu.se>
2026-03-19 13:39:26 +0100

    Highlight dephead

--- src/results/kwic/KwicToken.vue
+++ src/results/kwic/KwicToken.vue
@@ -1,13 +1,25 @@
 <script setup lang="ts">
 import { isKwicRowToken, isRowTokenEqual, type RowToken } from "@/core/kwic/kwic"
 import { injectionKeys } from "@/injection"
-import { inject } from "vue"
+import { isEqual } from "lodash-es"
+import { computed, inject } from "vue"
 
-defineProps<{
+const props = defineProps<{
   rowToken: RowToken
 }>()
 
 const selectedToken = inject(injectionKeys.selectedToken)
+
+const isDepheadToSelected = computed(() => {
+  const selected = selectedToken?.value
+  if (!isKwicRowToken(props.rowToken) || !selected || !isKwicRowToken(selected)) return false
+  if (!isEqual(props.rowToken.row, selected.row)) return false
+  const token = props.rowToken.token
+  if (!("dephead" in selected.token) || !(typeof selected.token.dephead == "string")) return false
+  const head = selected.token.dephead
+  if (!("ref" in token) || !(typeof token.ref == "string")) return false
+  return token.ref == head
+})
 </script>
 
 <template>
@@ -16,7 +28,8 @@ const selectedToken = inject(injectionKeys.selectedToken)
     class="kwic-token rounded-3"
     :class="{
       'fw-bold': isKwicRowToken(rowToken) && rowToken.token._match,
-      'kwic-token-selected': isRowTokenEqual(selectedToken, rowToken),
+      'bg-success-subtle text-success-emphasis': isRowTokenEqual(selectedToken, rowToken),
+      'bg-info-subtle text-info-emphasis': isDepheadToSelected,
     }"
     @click.stop="selectedToken = rowToken"
   >
@@ -27,16 +40,11 @@ const selectedToken = inject(injectionKeys.selectedToken)
 <style lang="scss">
 .kwic-token {
   cursor: pointer;
-  transition: 100ms ease-in-out;
+  transition: all 100ms ease-in-out;
 
   &:hover {
     background-color: var(--bs-primary-bg-subtle);
     color: var(--bs-primary-text-emphasis);
   }
-}
-
-.kwic-token-selected {
-  background-color: var(--bs-success-bg-subtle);
-  color: var(--bs-success-text-emphasis);
 }
 </style>
