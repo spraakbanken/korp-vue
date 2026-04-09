c7e7a504b3de14ee8762b19686ac09f1021c636b
Arild Matsson <arild.matsson@gu.se>
2026-04-09 16:58:44 +0200

    Sidebar details hover

--- src/components/DetailsDropdown.vue
+++ src/components/DetailsDropdown.vue
@@ -1,14 +1,24 @@
 <script lang="ts" setup>
-import { useId } from "vue"
+import { Dropdown } from "bootstrap"
+import { useId, useTemplateRef } from "vue"
+import { vElementHover } from "@vueuse/components"
 
 const id = useId()
+const toggle = useTemplateRef<HTMLButtonElement>("toggle")
+
+function onHover(state: boolean) {
+  const dropdown = Dropdown.getOrCreateInstance(toggle.value!)
+  if (state) dropdown.show()
+  else dropdown.hide()
+}
 </script>
 
 <template>
-  <div class="dropdown float-end">
+  <div class="dropdown float-end" v-element-hover="[onHover, { delayEnter: 300, delayLeave: 300 }]">
     <button
       type="button"
       :id
+      ref="toggle"
       class="btn btn-link p-0 small lh-1 text-secondary"
       data-bs-toggle="dropdown"
       data-bs-auto-close="outside"
